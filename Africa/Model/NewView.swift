3b8501cf63eec8e76356bbb30aa4856eb73b8a22
Aung Kyaw Mon <aungkyawmon.am@gmail.com>
2023-06-08 20:22:54 +0630

    - initial commit

---
+++ Africa/Model/NewView.swift
@@ -0,0 +1,20 @@
+//
+//  NewView.swift
+//  Africa
+//
+//  Created by Aung Kyaw Mon on 08/06/2566 BE.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView()
+    }
+}
